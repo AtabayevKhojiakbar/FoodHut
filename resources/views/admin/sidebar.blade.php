17a038283d5293b53081b0f506061c1c3a4babd4
Hojiakbar <hatabayev17@gmail.com>
2023-05-03 17:34:46 +0500

    admin page add

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,45 @@
+<div class="sidebar sidebar-style-2">
+    <div class="sidebar-wrapper scrollbar scrollbar-inner">
+        <div class="sidebar-content">
+{{--            <ul class="nav nav-primary">--}}
+{{--                <li class="nav-item {{  request()->routeIs('admin.users.index') ? 'active' : '' }}">--}}
+{{--                    <a href="{{route('admin.users.index')}}">--}}
+{{--                        <i class="fas fa-user"></i>--}}
+{{--                        <p>Foydalanuvchilar</p>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+
+{{--                <li class="nav-item {{  request()->routeIs('admin.photos.index') ? 'active' : '' }}">--}}
+{{--                    <a href="{{route('admin.photos.index')}}">--}}
+{{--                        <i class="fas fa-image"></i>--}}
+{{--                        <p>Fotogaleriya</p>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+
+{{--                <li class="nav-item {{  request()->routeIs('admin.shoir.index') ? 'active' : '' }}">--}}
+{{--                    <a href="{{route('admin.shoir.index')}}">--}}
+{{--                        <i class="fas fa-pen"></i>--}}
+{{--                        <p>Shoirlar</p>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+
+{{--                <li class="nav-item {{  request()->routeIs('admin.sher.index') ? 'active' : '' }}">--}}
+{{--                    <a href="{{route('admin.sher.index')}}">--}}
+{{--                        <i class="fas fa-bars"></i>--}}
+{{--                        <p>Sherlar</p>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li class="nav-item {{  request()->routeIs('admin.kitob.index') ? 'active' : '' }}">--}}
+{{--                    <a href="{{route('admin.kitob.index')}}">--}}
+{{--                        <i class="fas fa-book-open"></i>--}}
+{{--                        <p>Kitoblar</p>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+
+{{--            </ul>--}}
+        </div>
+    </div>
+</div>
+
+
+
