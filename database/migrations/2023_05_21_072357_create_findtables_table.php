bd65478c3e8f13538159895246fa5edee570ff32
Hojiakbar <hatabayev17@gmail.com>
2023-05-21 12:37:33 +0500

    migration add

--- database/migrations/2023_05_21_072357_create_findtables_table.php
+++ database/migrations/2023_05_21_072357_create_findtables_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('findtables', function (Blueprint $table) {
             $table->id();
+            $table->string('email');
+            $table->integer('number_guests');
+            $table->time('time');
+            $table->date('date');
             $table->timestamps();
         });
     }
